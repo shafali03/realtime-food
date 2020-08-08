9eab1d3195a6e5f8f7cfb31b232500a11272532a
shafali03 <shaf03sm@gmail.com>
2020-08-08 12:32:37 +0100

    feat: Created cart controller

--- routes/web.js
+++ routes/web.js
@@ -1,19 +1,15 @@
 // import
 const homeController = require('../app/http/controllers/homeController')
 const authController = require('../app/http/controllers/authController')
+const cartController = require('../app/http/controllers/customers/cartController')
 
 
 function initRoutes(app) {
-
+  // Routes
   app.get('/', homeController().index)
-
-  app.get('/cart', (req, res) => {
-    res.render('customers/cart')
-  })
-
+  app.get('/cart', cartController().index)
   app.get('/login', authController().login)
   app.get('/register', authController().register)
-
 }
 
 module.exports = initRoutes
