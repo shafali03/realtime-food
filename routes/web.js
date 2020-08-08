c2c8942a29b4ed3364cc79c476640c57296e17d2
shafali03 <shaf03sm@gmail.com>
2020-08-08 11:34:41 +0100

    feat: Created home controller

--- routes/web.js
+++ routes/web.js
@@ -1,9 +1,12 @@
+// import
+const homeController = require('../app/http/controllers/homeController')
+
 
 function initRoutes(app) {
 
-  app.get('/', (req, res) => {
-    res.render('home')
-  })
+
+  app.get('/', homeController().index)
+
 
   app.get('/cart', (req, res) => {
     res.render('customers/cart')
