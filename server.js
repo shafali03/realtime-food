f878d1b37a5acdb18544a41b49adefc74d53291c
shafali03 <shaf03sm@gmail.com>
2020-08-09 13:36:31 +0100

    fix: fixed session cookie removed the s from cookies

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(session({
   resave: false,
   store: mongoStore,
   saveUninitialized: false,
-  cookies: { maxAge: 1000 * 60 * 24 } // 24 hours
+  cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hours
 }))
 
 app.use(flash())
