7e4bbcdab4d8f2ae15d034e6dfec48475f3b63f4
shafali03 <shaf03sm@gmail.com>
2020-08-09 09:52:44 +0100

    feat: Created async await for homeController

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -1,12 +1,11 @@
 const Menu = require('../../models/menu')
 
 function homeController() {
+
   return {
-    index(req, res) {
-      Menu.find().then(function (pizzas) {
-        console.log(pizzas)
-        return res.render('home', { pizzas: pizzas })
-      })
+    async index(req, res) {
+      const pizzas = await Menu.find()
+      return res.render('home', { pizzas: pizzas })
     }
   }
 }
