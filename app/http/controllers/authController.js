54a91aef67ba608e68dabfb34bec8907958240e1
shafali03 <shaf03sm@gmail.com>
2020-08-12 13:22:43 +0100

    feat: Register information passed to mongoDB database

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,7 +1,8 @@
 
 const User = require('../../models/user')
-function authController() {
+const bcrypt = require('bcrypt')
 
+function authController() {
   return {
     login(req, res) {
       res.render('auth/login')
@@ -9,7 +10,8 @@ function authController() {
     register(req, res) {
       res.render('auth/register')
     },
-    postRegister(req, res) {
+
+    async postRegister(req, res) {
       const { name, email, password } = req.body
       // Validate request 
       if (!name || !email || !password) {
@@ -19,6 +21,36 @@ function authController() {
 
         return res.redirect('/register')
       }
+
+      // Check if email exists
+      User.exists({ email: email }, (err, result) => {
+        if (result) {
+          req.flash('error', 'Email already exist')
+          req.flash('name', name)
+          req.flash('email', email)
+          return res.redirect('/register')
+        }
+      })
+
+      // Hash password
+      const hashedPassword = await bcrypt.hash(password, 10)
+      // Create a user
+      const user = new User({
+        name,
+        email,
+        password: hashedPassword
+      })
+
+      user.save().then((user) => {
+        // login
+
+        return res.redirect('/')
+      }).catch(err => {
+        req.flash('error', 'Something went wrong')
+        return res.redirect('/register')
+
+      })
+
       console.log(req.body)
     }
   }
